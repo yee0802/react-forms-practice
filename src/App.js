bad91734d3077e2168c950e32f8d79a3da2fe051
Mike <mike.m.herron@googlemail.com>
2022-03-03 08:48:02 +0000

    Init exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  
+  //TODO: Add your state fields here
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="form">
+        <h2>Complaining form!</h2>
+        <div className="form__section-left">
+          <label>
+            Full name
+            <input type="text" name="name" required />
+          </label>
+          <label>
+            Address
+            <input type="text" name="address" />
+          </label>
+          <label>
+            Phone Number
+            <input type="telephone" name="phone" />
+          </label>
+
+          <label>
+            Email
+            <input type="email" name="email" />
+          </label>
+        </div>
+
+        <div className="form__section-right">
+          <label>
+            Write your complaint
+            <textarea
+              name="complaint"
+              rows="10"
+              placeholder="You can complain here"
+            ></textarea>
+          </label>
+
+          <div className="form__radio-group">
+            <p>How do you want to be contacted? </p>
+            <label>
+              <input type="radio" name="contact" value="phone" />
+              Phone
+            </label>
+
+            <label>
+              <input type="radio" name="contact" value="email" />
+              Email
+            </label>
+
+            <label>
+              <input type="radio" name="contact" value="post" />
+              Slow Mail
+            </label>
+
+            <label>
+              <input type="radio" name="contact" value="none" />
+              No contact!
+            </label>
+          </div>
+
+          <label>
+            I agree you take my data, and do whatever
+            <input type="checkbox" name="consent" id="consent" />
+          </label>
+        </div>
+        <input type="submit" value="Submit!" />
+      </form>
+    </>
   );
 }
-
-export default App;
