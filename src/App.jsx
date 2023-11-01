04087077a2f7b8a4be05f3238a399c069c5dea2e
Kye <NovusAa12@proton.me>
2023-11-01 12:04:00 +0000

    add form functionality (prac.)

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,58 @@
 import { useState } from "react";
 import "./App.css";
 
-export default function App() {
+const INITIAL_FORM_STATE = {
+  fullName: "",
+  address: "",
+  phone: "",
+  email: "",
+  complaint: "",
+  contact: "",
+  consent: false
+};
 
-  //TODO: Add your state fields here
+export default function App() {
+  const [ form, setForm ] = useState(INITIAL_FORM_STATE);
+  
+  const sumbitForm = (e) => {
+    e.preventDefault();
+
+    // fetch JSON server and add form
+    // reset form to inital state
+    // console.log(form)
+    setForm(INITIAL_FORM_STATE);
+  };
+
+  const handleUserInput = (e) => {
+    const { name, type, value, checked } = e.target;
+
+    type === "checkbox" ? setForm({ ...form, [name]: checked})
+    : setForm({ ...form, [name]: value })
+  };
+
+  const contactOpts = ["phone", "email", "post", "none"];
 
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={sumbitForm}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input type="text" name="fullName" required onChange={(e) => handleUserInput(e)} />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input type="text" name="address" onChange={(e) => handleUserInput(e)} />
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input type="tel" name="phone" onChange={(e) => handleUserInput(e)} />
           </label>
 
           <label>
             Email
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={(e) => handleUserInput(e)} />
           </label>
         </div>
 
@@ -36,35 +63,23 @@ export default function App() {
               name="complaint"
               rows="10"
               placeholder="You can complain here"
+              onChange={(e) => handleUserInput(e)}
             ></textarea>
           </label>
 
           <div className="form__radio-group">
             <p>How do you want to be contacted? </p>
-            <label>
-              <input type="radio" name="contact" value="phone" />
-              Phone
-            </label>
-
-            <label>
-              <input type="radio" name="contact" value="email" />
-              Email
-            </label>
-
-            <label>
-              <input type="radio" name="contact" value="post" />
-              Slow Mail
-            </label>
-
-            <label>
-              <input type="radio" name="contact" value="none" />
-              No contact!
-            </label>
+            {contactOpts.map(opt => 
+              <label key={opt}>
+                <input type="radio" name="contact" value={opt} onChange={(e) => handleUserInput(e)} />
+                {opt}
+              </label>
+              )}
           </div>
 
           <label>
             I agree you take my data, and do whatever
-            <input type="checkbox" name="consent" id="consent" />
+            <input type="checkbox" name="consent" id="consent" onChange={(e) => handleUserInput(e)} />
           </label>
         </div>
         <input type="submit" value="Submit!" />
